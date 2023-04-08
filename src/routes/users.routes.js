efbef30ec9eab96a03763e00ff2a3e725a35e6d8
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-08 11:48:03 -0300

    agrega .js a las rutas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getUsers, getUser, createUser, editUser, deleteUser } from '../controllers/user.controller'
+import { getUsers, getUser, createUser, editUser, deleteUser } from '../controllers/user.controller.js'
 
 const router = Router()
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express  from 'express'
 import cors from 'cors' 
+import router from './routes/users.routes.js'
 
 export class Server {
 
