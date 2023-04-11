0220a5d103de00d960e7c92f7854a607902c29ca
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-11 16:45:46 -0300

    agrega typo y mensaje al esquema de usuario

--- src/model/User.js
+++ src/model/User.js
@@ -1,10 +1,13 @@
-import { Schema } from "mongoose"
+import { Schema } from 'mongoose'
 
 const UserSchema = new Schema({
-  name: { type: String},
-  email: { type: String},
-  adress: { type: String },
-  password: { type: String },
-  isActive: { type: Boolean },
-  isAdmin: { type: Boolean },
+  name: { type: String, required:[true, 'El nombre es requerido']},
+  email: { type: String, required:[true, 'El mail es requerido']}, 
+  adress: { type: String, required:[true, 'La dirección es requerida'] },
+  PhoneNumber: { type: Number, required:[true, 'En número de teléfono es requerido']},
+  password: { type: String, required:[true, 'La contraseña es requerida']},
+  isActive: { type: Boolean,},
+  isAdmin: { type: Boolean }
 })
+
+export default model('User', UserSchema)
