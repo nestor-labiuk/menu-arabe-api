534bd849419ecbfc57e031633741212c7a8c27fc
Leandro Herrera <leandroherrera2008@gmail.com>
2023-04-21 19:35:54 -0300

    Implementación del modelo de pedido, de los controladores y de las rutas

---
+++ src/controllers/order.controllers.js
@@ -0,0 +1,30 @@
+import Order from '../model/Order.js'
+
+export const getOrders = async (req, res) => {
+  request.json('Pedidos obtenidos')
+}
+
+export const getOrder = (req, res) => {
+  const { id } = req.params
+  request.json(`Obtuviste el Pedido con id ${id}`)
+}
+
+export const createOrder = async (req, res) => {
+  const { name, state, menu, price, date } = req.body
+  const order = await Order({ name, state, menu, price, date })
+  try {
+    order.save()
+    res.status(201).json({
+      message: `Pedido ${name} creado`
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'No se pudo hacer el Pedido'
+    })
+    console.log(error)
+  }
+}
+
+export const deleteOrder = (req, res) => {
+  request.json('Borraste un Pedido')
+}
