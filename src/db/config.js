9255d7c00ff79530f012207e1cf515a21709af43
nestor-labiuk <nestorlabiuk@gmail.com>
2023-04-22 17:28:18 -0300

    agrega mensajes a las validaciones de usuarios

--- src/db/config.js
+++ src/db/config.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 export const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_CONNECTION)
-    console.log('Conexión exitosa')
+    console.log('La conexión con la base de datos fue exitosa')
   } catch (error) {
-    console.log('Falló la conexión')
+    console.log('Falló la conexión con  la base de datos')
   }
 }
