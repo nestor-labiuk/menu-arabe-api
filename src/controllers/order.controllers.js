0af06e3c5a7562c1ac6ce12caa11cae8fd862833
Leandro Herrera <leandroherrera2008@gmail.com>
2023-04-30 23:39:47 -0300

    actualizacion de los controladores de la administracion de pedidos

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -1,17 +1,51 @@
 import Order from '../model/Order.js'
 
+
 export const getOrders = async (req, res) => {
-  request.json('Pedidos obtenidos')
+  const [ orders, total ]  = await Promise.all([
+    Order.find({})
+  ])
+
+  if(total===0){
+    return res.status(404).json ({
+      message: "No hay pedidos",
+    })
+  }
+  if (orders){
+    return res.status(200).json ({
+      message: "Pedidos retornados exitosamente",
+      // total,
+      orders
+    })
+  }
+  
 }
 
-export const getOrder = (req, res) => {
+
+export const getOrder = async (req, res) => {
   const { id } = req.params
-  request.json(`Obtuviste el Pedido con id ${id}`)
+  if(!Order){
+    return res.status(500).json({
+      message: 'No se pudo obtener el Pedido'
+    })
+  }
+  req.json(`Obtuviste el Pedido con id ${id}`)
+
+  const order = await Orders.findById(id)
+    if(!order){
+      return res.status(404).json({
+        message: 'No se encontro el Pedido'
+      })
+    }
+    res.json({
+    order
+  })
 }
 
 export const createOrder = async (req, res) => {
-  const { name, state, menu, price, date } = req.body
-  const order = await Order({ name, state, menu, price, date })
+  const { name, state, menu, price, adress } = req.body
+  const order = await Order({ name, state, menu, price, adress })
+  
   try {
     order.save()
     res.status(201).json({
@@ -25,6 +59,21 @@ export const createOrder = async (req, res) => {
   }
 }
 
-export const deleteOrder = (req, res) => {
-  request.json('Borraste un Pedido')
+export const deleteOrder = async (req, res) => {  
+  const { id } = req.params
+  if(!Orders){
+    return res.status(404).json({
+      message: 'No se pudo borrar el Pedido'
+    })
+  }
+  const order = await Orders.findByIdAndDelete(id)
+  if(! order){
+    return res.status(404).json({
+      message: 'No hay Pedidos'
+    })
+  }
+  res.json({
+    message: `order: ${order?.name} eliminado exitosamente`
+  })
+  
 }
